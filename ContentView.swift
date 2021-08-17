00aedc3864aa606a8fff854c6f75dcecd0a872ec
José Francisco Castillo Rodríguez <josefrancasrod@gmail.com>
2021-08-16 21:36:52 -0500

    First Commit

---
+++ ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI Practice
+//
+//  Created by José Francisco Castillo Rodríguez on 16/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_PracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_PracticeApp.swift
+//  SwiftUI Practice
+//
+//  Created by José Francisco Castillo Rodríguez on 16/08/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_PracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
